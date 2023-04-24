ded00825acc69ab495e29326dad4bd0bc7701f23
vladberejn <vladberejn@gmail.com>
2023-04-24 18:59:24 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@mui/material'
+import { useState } from 'react'
+import MainMenu from './components/MainMenu'
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <MainMenu/>
+    </>
+  )
+}
+
+export default App
